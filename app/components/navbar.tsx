d65d763cdfeee79820765d0e565c205f9c8caf85
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2025-03-10 07:43:17 -0400

    feat: documentation updates

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    < header className="sticky top-0 inset-x-0 bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full" >
+    <header className="sticky top-0 inset-x-0 bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full" >
       <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 px-4 sm:px-6 lg:px-8 py-2">
         {/* Logo w/ Collapse Button */}
         <div className="flex items-center justify-between">
@@ -30,6 +30,6 @@ export default function NavBar() {
         </div>
         {/* End Collapse */}
       </nav>
-    </header >
+    </header>
   )
 }
